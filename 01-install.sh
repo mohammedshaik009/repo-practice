52494b1ab860805cb3a444ca40140138ea4804c8
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 10:22:29 +0530

    shell script started

--- 01-install.sh
+++ 01-install.sh
@@ -3,8 +3,7 @@
 USERID=$(id -u)
 #check root access or not
 
-if [ $USERID -ne 0 ];then
+if [$USERID -ne 0 ];then
     echo "please run this script with root access"
-fi 
-
-echo "im continiung"
+fi
+echo "im continuing"
