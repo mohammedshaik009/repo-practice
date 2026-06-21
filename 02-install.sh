e36be3cb99942d6cabe14c93c069d98a1443a3d9
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 11:06:42 +0530

    shell script started

--- 02-install.sh
+++ 02-install.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
-#check root access or not
+#checking root access or not 
 
 if [ $USERID -ne 0 ];then
     echo "please run this script with root access"
-fi
-
-echo "installing mysql"
-    dnf install mysql -y
-
-if [ $? -ne 0 ];then
-    echo "installing MYSQL is ...FAILED"
-    exit 1
-else 
-    echo "installing mysql is ... SUCCESS"
-fi
+fi 
+exit 1
