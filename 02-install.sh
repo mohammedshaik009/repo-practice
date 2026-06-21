4b28670b9e81ee3bb175e86f26f26aca39610a8c
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 11:17:33 +0530

    shell script started

--- 02-install.sh
+++ 02-install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
-#checking root access or not 
+USERID=(id -u)
+#check root access or not
 
 if [ $USERID -ne 0 ];then
     echo "please run this script with root access"
 fi 
-
-echo "removing MYSQL"
-dnf remove mysql -y
+exit 1
