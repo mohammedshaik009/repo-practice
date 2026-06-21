20f384d30ca74af2dc531dd17fae3454c6bbc509
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-06-21 11:09:48 +0530

    shell script started

--- 02-install.sh
+++ 02-install.sh
@@ -7,3 +7,12 @@ if [ $USERID -ne 0 ];then
     echo "please run this script with root access"
 fi 
 exit 1
+
+echo "removing MYSQL"
+dnf remove mysql -y
+
+if [ $? -1];then
+    echo "removing mysql is sucess"
+else 
+    echo "removing mysql is failed"
+fi
